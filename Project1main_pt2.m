251fd85ff4768e581960e25eb82340b30f81e1b8
Guilherme de Sousa <72522051+gdesousa2@users.noreply.github.com>
2020-10-08 18:57:33 -0400

    Add files via upload

--- Project1main_pt2.m
+++ Project1main_pt2.m
@@ -388,19 +388,19 @@ Y = (label*ones(1,dim + 1)).*[XX,ones(n,1)];
 w0 = [-1;-1;1;1]; % initial guess
 
 t1 = tic;
-[wLB1,fLB1,gnormLB1] = SG_st1(@(w)fun0((1:n)',Y,w,lam(1)),@(I,Y,w)gfun0(I,Y,w,lam(1)),Y,bsz(1),w0,samp,step);
+[wLB1,fLB1,gnormLB1] = SG_st1(@(w)fun0((1:n)',Y,w,lam(1)),@(I,Y,w)gfun0(I,Y,w,lam(1)),Y,bsz,w0,samp,step);
 t1 = toc(t1);
 t2 = tic;
-[wLB2,fLB2,gnormLB2] = SG_st1(@(w)fun0((1:n)',Y,w,lam(2)),@(I,Y,w)gfun0(I,Y,w,lam(2)),Y,bsz(1),w0,samp,step);
+[wLB2,fLB2,gnormLB2] = SG_st1(@(w)fun0((1:n)',Y,w,lam(2)),@(I,Y,w)gfun0(I,Y,w,lam(2)),Y,bsz,w0,samp,step);
 t2 = toc(t2);
 t3 = tic;
-[wLB3,fLB3,gnormLB3] = SG_st1(@(w)fun0((1:n)',Y,w,lam(3)),@(I,Y,w)gfun0(I,Y,w,lam(3)),Y,bsz(1),w0,samp,step);
+[wLB3,fLB3,gnormLB3] = SG_st1(@(w)fun0((1:n)',Y,w,lam(3)),@(I,Y,w)gfun0(I,Y,w,lam(3)),Y,bsz,w0,samp,step);
 t3 = toc(t3);
 t4 = tic;
-[wLB4,fLB4,gnormLB4] = SG_st1(@(w)fun0((1:n)',Y,w,lam(4)),@(I,Y,w)gfun0(I,Y,w,lam(4)),Y,bsz(1),w0,samp,step);
+[wLB4,fLB4,gnormLB4] = SG_st1(@(w)fun0((1:n)',Y,w,lam(4)),@(I,Y,w)gfun0(I,Y,w,lam(4)),Y,bsz,w0,samp,step);
 t4 = toc(t4);
 t5 = tic;
-[wLB5,fLB5,gnormLB5] = SG_st1(@(w)fun0((1:n)',Y,w,lam(5)),@(I,Y,w)gfun0(I,Y,w,lam(5)),Y,bsz(1),w0,samp,step);
+[wLB5,fLB5,gnormLB5] = SG_st1(@(w)fun0((1:n)',Y,w,lam(5)),@(I,Y,w)gfun0(I,Y,w,lam(5)),Y,bsz,w0,samp,step);
 t5 = toc(t5);
 
 runtime = [t1; t2; t3; t4; t5]; % run time for different batch sizes
